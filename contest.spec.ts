a09055df7fd4e8f26c2b8b4dbc48edd3df78d5a2
Noel Jacob <noeljacob91@gmail.com>
2022-12-06 15:49:47 +0530

    1

--- contest.spec.ts
+++ contest.spec.ts
@@ -32,30 +32,37 @@ describe('1', () => {
         //     big_cell = beginCell().storeUint(0, 1023).storeRef(big_cell).endCell();
         // }
 
-        const cell_size = 2;
-        let recursive = (cells, bits, refs, refs_total) => {
-            if (bits < 998976) {
-                if (refs_total < 254) {
-                    if (refs < 4) {
-                    cells += 1;
-                    refs_total += 1;
-                    bits += cell_size;
-                    refs += 1;
-                    let res = beginCell().storeUint(0, cell_size).storeRef(recursive(cells, bits, refs, refs_total));
-                    return res.endCell();
+        let rand = (y) => {
+            let r = Math.random();
+            return new Array(Math.round(r * y)).fill(0).map(() => (r >= .5) ? 1 : 0)
+        }
+
+        let recursive = (c, b, d) => {
+            let buf = rand(10);
+            let bui = beginCell().storeBitArray(buf)
+            if (b < 10000 && c < 40) {
+                if (d < 3) {
+                    c += 1;
+                    b += buf.length;
+                    d += 1;
+                    for (let r = 0; b < 10000 && c < 40 && r < 4; ++r) {
+                        c += 1;
+                        b += buf.length;
+                        bui = bui.storeRef(recursive(c, b, d))
                     }
                 }
             }
-            console.log("Cells: " + (cells + 1), "Bits: " + (bits + 1023), "Refs: " + refs_total);
-            return beginCell().storeUint(0, 1023).endCell();
+            console.log("Cells: " + (c + 1), "Bits: " + (b + buf.length), "Depth: " + d);
+            return bui.endCell();
         }
-        let big_cell = recursive(0, 0, 0, 0);
+        let big_cell = recursive(0, 0, 0);
+        console.log(big_cell.refs);
 
         let result = await x.invokeGetMethod("decomposite", [stackCell(big_cell), stackSlice(destination_address)])
-        let [cells, bits, refs] = result.result[0]
-        // expect(cells).toBe(1)
-        // expect(bits).toBe(8)
-        // expect(refs).toBe(0)
-        console.log("Cells:" + cells.toString(), "Bits:" + bits.toString(), "Refs:" + refs.toString())
+        let [cells, bits, depth] = result.result[0]
+        // expect(cells).toBe(c)
+        // expect(bits).toBe(b)
+        // expect(depth).toBe(d)
+        console.log("Cells:" + cells.toString(), "Bits:" + bits.toString(), "Depth:" + depth.toString())
     })
 })
